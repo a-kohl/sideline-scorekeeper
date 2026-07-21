82683ed6a0c1f7a8c89597618b72374dd0b7c8b9
Andrew Kohl <andy@akoperations.com>
2026-07-21 14:52:18 -0500

    Add Sideline Scorekeeper PWA
    Installable scorekeeper for flag football and soccer with offline support via manifest + service worker.

---
+++ sw.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var CACHE_NAME = "sideline-scorekeeper-v1";
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){ return cache.addAll(ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      return cached || fetch(event.request);
+    })
+  );
+});
